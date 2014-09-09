cd189156d7d42b86b29e81220dc09b7e7f019ad2
Ryo Sugimoto <sugryo1109@gmail.com>
2014-09-09 16:38:41 +0900

    Add messages

--- download.rb
+++ download.rb
@@ -2,11 +2,13 @@ require "rss"
 
 rss = RSS::Parser.parse("http://feeds.rebuild.fm/rebuildfm")
 
+puts "------ Start downloading ------"
 rss.items.each do |item|
-  puts item.title
+  puts "Now download -> #{item.title}"
   system("curl",
 	 "--location",
 	 "--output",
 	 "#{item.title}.mp3",
 	 "#{item.enclosure.url}")
 end
+puts "------ Finished downloading ------"
