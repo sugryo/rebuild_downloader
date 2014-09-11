7aa1eded933345d545dba9579d68947da2cba4b8
Ryo Sugimoto <sugryo1109@gmail.com>
2014-09-11 18:57:52 +0900

    Add the download_directory

--- download.rb
+++ download.rb
@@ -3,12 +3,15 @@ require "rss"
 class Rebuild
   def initialize
     @rss = RSS::Parser.parse("http://feeds.rebuild.fm/rebuildfm")
+    @home = Dir.home
   end
 
   def download
     puts "------ Start downloading ------"
     @rss.items.each do |item|
       puts "Now download -> #{item.title}"
+      system("cd",
+	     "#{download_directory}")
       system("curl",
 	     "--location",
 	     "--output",
@@ -16,6 +19,19 @@ class Rebuild
 	     "#{item.enclosure.url}")
     end
     puts "****** Finished downloading ******"
+  end
+
+  private
+  def download_directory
+    "#{@home}/rebuild"
+  end
+
+  def create_directory
+    begin
+      Dir.chdir(download_directory)
+    rescue Errno::ENOENT
+      Dir.mkdir(download_directory)
+    end
   end
 end
 
