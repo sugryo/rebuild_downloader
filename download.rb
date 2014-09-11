e67774e1f9dc0e4d70f35bd522e60c4d3a4d61b6
Ryo Sugimoto <sugryo1109@gmail.com>
2014-09-11 18:13:43 +0900

    Add the Rebuild class

--- download.rb
+++ download.rb
@@ -12,3 +12,22 @@ rss.items.each do |item|
 	 "#{item.enclosure.url}")
 end
 puts "------ Finished downloading ------"
+
+class Rebuild
+  def initialize
+    @rss = RSS::Parser.parse("http://feeds.rebuild.fm/rebuildfm")
+  end
+
+  def download
+    puts "------ Start downloading ------"
+    @rss.items.each do |item|
+      puts "Now download -> #{item.title}"
+      system("curl",
+	     "--location",
+	     "--output",
+	     "#{item.title}.mp3",
+	     "#{item.enclosure.url}")
+    end
+    puts "****** Finished downloading ******"
+  end
+end
